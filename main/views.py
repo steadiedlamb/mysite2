513edb638e96067765302b87e039526ced9f9ed2
Max <maxwouters97@gmail.com>
2024-05-09 12:55:37 +0200

    MW: First Commit.

---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList, Item
+from .forms import CreateNewList
+# Create your views here.
+
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html", {"ls":ls})
+
+def home(response):
+    return render(response, "main/home.html", {})
+
+def create(response):
+    form = CreateNewList()
+    return render(response, "main/create.html", {"form":form})
+
+def meet_team(response):
+    return render(response, "main/team.html", {})
